eb8b85ba3d65a88266aa96da6b8d59e235961a6d
coramdeo643 <coramdeo643@kakao.com>
2025-05-19 11:25:03 +0900

    250519.1100.BookDAO class completed

--- src/dao/BookDAO.java
+++ src/dao/BookDAO.java
@@ -38,7 +38,7 @@ public class BookDAO {
                 = "select * from books";
         try (Connection conn = DatabaseUtil.getConnection();
              Statement stmt = conn.createStatement()) {
-            // vody
+            // body
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 int id = rs.getInt("id");
@@ -52,15 +52,36 @@ public class BookDAO {
                 Book book = new Book(id, title, author, publisher, publicationYear, isbn, available);
                 bookList.add(book);
                 // bookList.add(new Book(id, title, author, publisher, publicationYear, isbn, available));
-
             }
         }
-
         return bookList;
     }
 
     // 책 제목으로 도서 검색 기능 select
-    public void searchTitle() {
+    public List<Book> searchBooksTitle(String searchTitle) throws SQLException {
+        List<Book> bookList = new ArrayList<>();
+        String sql
+                = "select * from books where title like ? ";
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            // body
+            pstmt.setString(1, "%" + searchTitle + "%");
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String author = rs.getString("author");
+                String publisher = rs.getString("publisher");
+                int publicationYear = rs.getInt("publication_year");
+                String isbn = rs.getString("isbn");
+                boolean available = rs.getBoolean("available");
+
+                Book book = new Book(id, title, author, publisher, publicationYear, isbn, available);
+                bookList.add(book);
+            }
+        }
+        return bookList;
     }
 
     // 수정 / 삭제 기능 update / delete
@@ -70,18 +91,24 @@ public class BookDAO {
     public void deleteBook() {
     }
 
+    //TODO 삭제 예정
     //test code
     public static void main(String[] args) {
         // all select test
         BookDAO bookDAO = new BookDAO();
-        try {
-            bookDAO.getAllBooks();
-            for (int i = 0; i < bookDAO.getAllBooks().size(); i++) {
-                System.out.println(bookDAO.getAllBooks().get(i));
-            }
-            bookDAO.addBook(new Book(0, "10", "10", "10", 2025, "10",true));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            bookDAO.getAllBooks();
+//            ArrayList<Book> selectedBookList
+//                    = (ArrayList) bookDAO.searchBooksTitle("입");
+//            for (int i = 0; i < selectedBookList.size(); i++) {
+//                System.out.println(selectedBookList.get(i));
+//            }
+//            bookDAO.addBook(new Book
+//                    (0, "10", "10", "10",2025, "10", true));
+//
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 } // end of class
